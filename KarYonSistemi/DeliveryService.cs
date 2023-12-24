7ce399fc582ff1b96389ecd8f66933d4aab2ea2f
Abdullah D <abdeadekebo@gmail.com>
2023-12-25 01:10:58 +0300

    feat: Refactor DeliveryService class to use abstract method for SendCargo

--- KarYonSistemi/DeliveryService.cs
+++ KarYonSistemi/DeliveryService.cs
@@ -5,9 +5,13 @@ namespace KarYonSistemi
     // Abstract class DeliveryService implementing IDeliveryService
     public abstract class DeliveryService : IDeliveryService
     {
-        public virtual async Task SendCargo(ShipmentInfo shipmentInfo)
-        {
-            // To be implemented by the derived classes
-        }
+        // Abstract method to be implemented by the derived classes
+        public abstract Task SendCargo(ShipmentInfo shipmentInfo);
+
+        //Or we can use virtual method instead of abstract method, when a default implementation is possible
+        //public virtual async Task SendCargo(ShipmentInfo shipmentInfo)
+        //{
+        //    // To be implemented by the derived classes
+        //}
     }
 }
